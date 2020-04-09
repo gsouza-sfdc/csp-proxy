a0b8f23204d8128286ba6d6e062b254ce1f2c53f
Gustavo Souza <gsouza@salesforce.com>
2020-04-09 07:21:37 -0700

    Fix cert option name, port number parsing, misc fixes

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ import https from 'https'
 import httpProxy from 'http-proxy';
 
 program
-    .option('-c, --certificate <certificate>', 'path to cert', 'cert.pem')
+    .option('-c, --cert <cert>', 'path to cert', 'cert.pem')
     .option('-d, --debug', 'output debugging info')
     .option('-k, --key <key>', 'path to cert key', 'key.pem')
-    .option('-p, --port <port>', 'port number to listen on', parseInt, 3000)
+    .option('-p, --port <port>', 'port number to listen on', (val) => parseInt(val), 3000)
     .option('-s, --ssl', 'use use https', false)
-    .option('-t, --target <url>', 'target server url', 'https://trailhead.salesforce.com/en/home')
+    .option('-t, --target <url>', 'target server url', 'https://trailhead.salesforce.com')
     .parse(process.argv);
 
 const { cert, debug, key, port, ssl, target } = program;
@@ -19,25 +19,26 @@ if (debug) {
     console.log('csp-proxy started with the following options:\n', program.opts());
 }
 
+let options = {};
+let protocol = null;
+
+if (ssl) {
+    protocol = https;
+    Object.assign(options, {
+        key: fs.readFileSync(key),
+        cert: fs.readFileSync(cert)
+    });
+} else {
+    protocol = http;
+}
+
 const proxy = httpProxy.createProxyServer({});
 
 proxy.on('proxyReq', (proxyReq, req, res) => {
     // This only works if you're adding new headers.
     // Rewriting or removing existing headers doesn't work here.
-    res.setHeader('x-foo', 'bar');
+    res.setHeader('x-forwarded-by', 'csp-proxy');
 });
-
-let protocol, options;
-if (ssl) {
-    protocol = https;
-    options = {
-        key: fs.readFileSync(key),
-        cert: fs.readFileSync(cert)
-    }
-} else {
-    protocol = http;
-    options = {};
-}
 
 protocol['createServer'](options, (req, res) => {
     // Save original writeHead method.
@@ -45,7 +46,8 @@ protocol['createServer'](options, (req, res) => {
 
     // Override res.writeHead with custom logic that manipulates headers.
     res.writeHead = (...args) => {
-        // Rewrite or remove headers here.
+        // Rewrite or remove headers. In this case we need to remove
+        // headers that prevent loading the target url in an iframe.
         res.removeHeader('content-security-policy');
         res.removeHeader('x-frame-options');
 
